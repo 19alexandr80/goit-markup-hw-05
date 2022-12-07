e5ad4bd0adc0ba481e7bf804ad12603aba880e2c
19alexandr80 <usanka1980@gmail.com>
2022-12-07 21:00:14 +0200

    closeModal

--- js/modal.js
+++ js/modal.js
@@ -4,12 +4,10 @@
       closeModalBtn: document.querySelector(".close-modal"),
       modal: document.querySelector(".modal"),
     };
-    console.log(refs.openModalBtn)
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
-        console.log(refs.modal)
       refs.modal.classList.toggle("is-hidden");
       refs.modal.classList.toggle("modal");
     }
